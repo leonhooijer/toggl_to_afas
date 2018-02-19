6b0593bdbf6abd2984541b7e021a0399bedc1ee0
Leon Hooijer <leonhooijer@outlook.com>
2018-02-19 09:39:36 +0100

    Use Time instead of DateTime

--- import_working_hours.rb
+++ import_working_hours.rb
@@ -13,8 +13,8 @@ begin
 
   # Read environment
   toggl_api_token = ENV.fetch("TOGGL_API_TOKEN").freeze
-  toggl_reports_since = DateTime.parse(ENV.fetch("SINCE")).to_datetime
-  toggl_reports_until = DateTime.parse(ENV.fetch("UNTIL")).to_datetime
+  toggl_reports_since = Time.parse(ENV.fetch("SINCE")).to_datetime
+  toggl_reports_until = Time.parse(ENV.fetch("UNTIL")).to_datetime
 
   # Toggl
   toggl_api = TogglV8::API.new(toggl_api_token)
@@ -34,7 +34,7 @@ begin
     project = toggl_record["project"]
     tags = toggl_record["tags"]
     duration = toggl_record["dur"]
-    start_time = DateTime.parse(toggl_record["start"])
+    start_time = Time.parse(toggl_record["start"])
 
     afas_project = /\((.*)\)/.match(project).to_a[1]
     afas_description = description
